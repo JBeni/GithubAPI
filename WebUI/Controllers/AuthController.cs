e4d7600c02e723f9c764223d9621fd411a8116a5
JBeni <jitca.beniamin@gmail.com>
2021-04-08 20:50:55 +0300

    Add project files.

---
+++ WebUI/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    //[ApiController]
+    [Route("[controller]/[action]")]
+    //[Authorize]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}

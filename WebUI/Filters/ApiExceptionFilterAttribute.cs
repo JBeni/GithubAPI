cd5849614f6758589e4d8926e9c893236ac9a8a6
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:13:54 +0300

    create filters for exceptions

---
+++ WebUI/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+
+namespace WebUI.Filters
+{
+    public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+        public ApiExceptionFilterAttribute()
+        {
+            _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>();
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+            HandleException(context);
+        }
+
+        private void HandleException(ExceptionContext context)
+        {
+            Type type = context.Exception.GetType();
+            if (_exceptionHandlers.ContainsKey(type))
+            {
+                _exceptionHandlers[type].Invoke(context);
+                return;
+            }
+
+            HandleUnknownException(context);
+        }
+
+        private void HandleUnknownException(ExceptionContext context)
+        {
+            var details = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = context.Exception.Message ?? "An error occurred while processing your request",
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+            };
+            context.Result = new ObjectResult(details)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}

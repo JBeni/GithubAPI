d9bca685a05b770c18f52db1e7dc0d6a35687280
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:09:02 +0300

    create Activity service

---
+++ Application/Interfaces/IActivityService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Handlers.Activities.Queries;
+using Application.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IActivityService
+    {
+        Task<List<EventResponse>> GetEvents(GetEventsQuery query);
+    }
+}
---
+++ Infrastructure/Services/ActivityService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Handlers.Activities.Queries;
+using Application.Interfaces;
+using Application.Responses;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class ActivityService : CommonService, IActivityService
+    {
+        public async Task<List<EventResponse>> GetEvents(GetEventsQuery query)
+        {
+            var response = await _httpClient.GetAsync("events");
+            var content = await response.Content.ReadAsStringAsync();
+            var gists = JsonConvert.DeserializeObject<List<EventResponse>>(content);
+            return gists;
+        }
+    }
+}

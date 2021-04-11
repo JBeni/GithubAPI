1dca3e4da4419bcb1f196fb163588b9ac367613e
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:07:21 +0300

    create GetGistsQuery

---
+++ Application/Handlers/Activities/Queries/GetEventsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetEventsQuery : IRequest<List<EventResponse>>
+    {
+    }
+
+    public class GetEventsQueryHandler : IRequestHandler<GetEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _eventService;
+
+        public GetEventsQueryHandler(IActivityService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _eventService.GetEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the events", ex);
+            }
+        }
+    }
+}

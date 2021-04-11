ec079c9d072f2cda7bbd4081cd2307be7a88074e
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:10:12 +0300

    create service for instantiated the HttpClient (for making request)

---
+++ Infrastructure/Services/CommonService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Infrastructure.Services
+{
+    public class CommonService
+    {
+        protected HttpClient _httpClient;
+
+        public CommonService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.github.com/")
+            };
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GithubAPI", "v1"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}

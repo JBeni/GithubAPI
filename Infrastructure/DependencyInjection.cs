d9d9c0ff52f002a33d88353c20a66c8894e22ccc
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:13:22 +0300

    inject services to DI - Infrastructure

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Interfaces;
+using Application.Interfaces;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IActivityService, ActivityService>();
+            services.AddTransient<IGistService, GistService>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+
+            return services;
+        }
+    }
+}

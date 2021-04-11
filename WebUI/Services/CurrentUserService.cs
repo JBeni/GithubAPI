d4ffd19c7eaa74570d9a05b02fa299bc07914ff8
JBeni <jitca.beniamin@gmail.com>
2021-04-11 13:54:59 +0300

    add CurrentUser service

---
+++ WebUI/Services/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace WebUI.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+
+        }
+
+        public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue("UserId");
+        public string Username => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+    }
+}

8af6e542572c3329c9f7d8abf0afda4119c08f14
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:09:22 +0300

    create Gist service

---
+++ Application/Interfaces/IGistService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Handlers.Gists.Queries;
+using Application.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IGistService
+    {
+        Task<List<GistResponse>> GetGists(GetGistsQuery query);
+    }
+}
---
+++ Infrastructure/Services/GistService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Handlers.Gists.Queries;
+using Application.Interfaces;
+using Application.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class GistService : IGistService
+    {
+        public Task<List<GistResponse>> GetGists(GetGistsQuery query)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

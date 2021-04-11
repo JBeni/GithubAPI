c60a2d077a3ce044779d2fb72952ed73376343e9
JBeni <jitca.beniamin@gmail.com>
2021-04-11 15:12:06 +0300

    create Gist and Event Responses

---
+++ Application/Responses/EventResponse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Application.Responses
+{
+    public class EventResponse
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public Actor Actor { get; set; }
+        public Repo Repo { get; set; }
+        public Payload Payload { get; set; }
+        public bool Public { get; set; }
+        public DateTime Created_at { get; set; }
+    }
+
+    public class Actor
+    {
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Display_login { get; set; }
+        public string Gravatar_id { get; set; }
+        public string Url { get; set; }
+        public string Avatar_url { get; set; }
+    }
+
+    public class Repo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Payload
+    {
+        public string Push_id { get; set; }
+        public int Size { get; set; }
+        public int Distinct_size { get; set; }
+        public string Ref { get; set; }
+        public string Head { get; set; }
+        public string Before { get; set; }
+    }
+
+    public class Commit
+    {
+        public string Sha { get; set; }
+        public Author Author { get; set; }
+        public string Message { get; set; }
+        public string Distinct { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Author
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Application/Responses/GistResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Application.Responses
+{
+    public class GistResponse
+    {
+        public string Url { get; set; }
+        public string Forks_url { get; set; }
+        public string Commits_url { get; set; }
+        public string Id { get; set; }
+        public string Node_id { get; set; }
+        public string Git_pull_url { get; set; }
+        public string Git_push_url { get; set; }
+        public string Html_url { get; set; }
+        public bool Public { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public string Description { get; set; }
+        public int Comments { get; set; }
+        public string User { get; set; }
+        public string Comments_url { get; set; }
+        public Owner Owner { get; set; }
+        public bool Truncated { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Login { get; set; }
+        public string Id { get; set; }
+        public string Node_id { get; set; }
+        public string Avatar_url { get; set; }
+        public string Gravatar_id { get; set; }
+        public string Url { get; set; }
+        public string Html_url { get; set; }
+        public string Followers_url { get; set; }
+        public string Following_url { get; set; }
+        public string Gists_url { get; set; }
+        public string Starred_url { get; set; }
+        public string Subscriptions_url { get; set; }
+        public string Organizations_url { get; set; }
+        public string Repos_url { get; set; }
+        public string Events_url { get; set; }
+        public string Received_events_url { get; set; }
+        public string Type { get; set; }
+        public bool Site_admin { get; set; }
+    }
+
+}

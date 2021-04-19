ad43c71b2310f4b91bfaf537c625fab37adfb2ab
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:09:19 +0300

    implement import status query

---
+++ Application/Handlers/Migrations/Queries/GetImportStatusQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Migrations.Queries
+{
+    public class GetImportStatusQuery : IRequest<ImportStatusResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+    }
+
+    public class GetImportStatusQueryHandler : IRequestHandler<GetImportStatusQuery, ImportStatusResponse>
+    {
+        private readonly IMigrationService _migrationService;
+
+        public GetImportStatusQueryHandler(IMigrationService migrationService)
+        {
+            _migrationService = migrationService;
+        }
+
+        public Task<ImportStatusResponse> Handle(GetImportStatusQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _migrationService.GetImportStatus(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the import status", ex);
+            }
+        }
+    }
+}

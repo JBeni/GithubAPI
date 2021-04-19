e9948a5d335a85009206cb3642912ddd0c58d148
JBeni <JBeni@users.noreply.github.com>
2021-04-19 19:10:24 +0300

    create methods for Gitignore controller

--- WebUI/Controllers/GitignoresController.cs
+++ WebUI/Controllers/GitignoresController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Handlers.Gitignores.Queries;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
     public class GitignoresController : ApiControllerBase
     {
-        [HttpGet("get-gists")]
-        public async Task<IActionResult> GetGists()
+        [HttpGet("get-gitignore-templates")]
+        public async Task<IActionResult> GetGitignoreTemplates(GetGitignoreTemplatesQuery query)
         {
-            return Ok();
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-gitignore-template")]
+        public async Task<IActionResult> GetGitignoreTemplate(GetGitignoreTemplateQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
         }
     }
 }

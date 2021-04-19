a950a8cf5fda393ce3af345dad64b456cd3ff4be
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:23:54 +0300

    create GetWorkflow Query

---
+++ Application/Handlers/Actions/Queries/GetWorkflowQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Actions.Queries
+{
+    public class GetWorkflowQuery : IRequest<WorkflowResponse>
+    {
+        public int WorkflowId { get; set; }
+    }
+
+    public class GetWorkflowQueryHandler : IRequestHandler<GetWorkflowQuery, WorkflowResponse>
+    {
+        private readonly IActionService _actionService;
+
+        public GetWorkflowQueryHandler(IActionService actionService)
+        {
+            _actionService = actionService;
+        }
+
+        public Task<WorkflowResponse> Handle(GetWorkflowQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _actionService.GetWorkflow(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the repository workflow", ex);
+            }
+        }
+    }
+}

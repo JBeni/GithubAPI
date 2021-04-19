3e9bb0d594ff6a4a5a4b275c1f3ded0600649782
JBeni <JBeni@users.noreply.github.com>
2021-04-19 19:09:52 +0300

    create methods for Activity controller

--- WebUI/Controllers/ActivitiesController.cs
+++ WebUI/Controllers/ActivitiesController.cs
@@ -7,7 +7,42 @@ namespace WebUI.Controllers
     public class ActivitiesController : ApiControllerBase
     {
         [HttpGet("get-events")]
-        public async Task<IActionResult> GetEvents([FromQuery] GetEventsQuery query)
+        public async Task<IActionResult> GetEvents(GetEventsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-network-public-events")]
+        public async Task<IActionResult> GetNetworkPublicEvents(GetNetworkPublicEventsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-organization-public-events")]
+        public async Task<IActionResult> GetOrganizationPublicEvents(GetOrganizationPublicEventsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-repository-events")]
+        public async Task<IActionResult> GetRepositoryEvents(GetRepositoryEventsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-user-public-events")]
+        public async Task<IActionResult> GetUserPublicEvents(GetUserPublicEventsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-user-received-public-events")]
+        public async Task<IActionResult> GetUserReceivedPublicEvents(GetUserReceivedPublicEventsQuery query)
         {
             var result = await Mediator.Send(query);
             return Ok(result);

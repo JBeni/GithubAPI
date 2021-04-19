866b6387f38d609331d91bcd8ca279545f7ce8bd
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:27:09 +0300

    create Gitignore Service

---
+++ Application/Interfaces/IGitignoreService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IGitignoreService
+    {
+        //Task<List<EventResponse>> GetGitignoreTemplates(GetGitignoreTemplatesQuery query);
+        //Task<List<EventResponse>> GetGitignoreTemplate(GetGitignoreTemplateQuery query);
+    }
+}

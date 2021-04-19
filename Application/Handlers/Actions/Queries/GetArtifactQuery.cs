02c7918d8fc21787afa4fcf695b6677f8c3dad56
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:23:00 +0300

    create GetArtifact Query

---
+++ Application/Handlers/Actions/Queries/GetArtifactQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Actions.Queries
+{
+    public class GetArtifactQuery : IRequest<ArtifactResponse>
+    {
+        public int ArtifactId { get; set; }
+    }
+
+    public class GetArtifactQueryHandler : IRequestHandler<GetArtifactQuery, ArtifactResponse>
+    {
+        private readonly IActionService _actionService;
+
+        public GetArtifactQueryHandler(IActionService actionService)
+        {
+            _actionService = actionService;
+        }
+
+        public Task<ArtifactResponse> Handle(GetArtifactQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _actionService.GetArtifact(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the repository artifact", ex);
+            }
+        }
+    }
+
+}

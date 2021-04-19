fb4343d5e1eca8689b099bde15e6f9d0e267fcbb
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:09:31 +0300

    implement user migrations query

---
+++ Application/Handlers/Migrations/Queries/GetUserMigrationsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Migrations.Queries
+{
+    public class GetUserMigrationsQuery : IRequest<List<UserMigrationResponse>>
+    {
+    }
+
+    public class GetUserMigrationsQueryHandler : IRequestHandler<GetUserMigrationsQuery, List<UserMigrationResponse>>
+    {
+        private readonly IMigrationService _migrationService;
+
+        public GetUserMigrationsQueryHandler(IMigrationService migrationService)
+        {
+            _migrationService = migrationService;
+        }
+
+        public Task<List<UserMigrationResponse>> Handle(GetUserMigrationsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _migrationService.GetUserMigrations(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the user migrations", ex);
+            }
+        }
+    }
+}

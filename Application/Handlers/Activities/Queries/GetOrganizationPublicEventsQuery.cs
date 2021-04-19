1e5fc014c1712addb924b2269e2e1308b9738fee
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:05:02 +0300

    implement  repository events query

---
+++ Application/Handlers/Activities/Queries/GetOrganizationPublicEventsQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetOrganizationPublicEventsQuery : IRequest<List<EventResponse>>
+    {
+        public string Organization { get; set; }
+    }
+
+    public class GetOrganizationPublicEventsHandler : IRequestHandler<GetOrganizationPublicEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _activityService;
+
+        public GetOrganizationPublicEventsHandler(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetOrganizationPublicEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _activityService.GetOrganizationPublicEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the organization public events", ex);
+            }
+        }
+    }
+}

936475454924ec591d02e398479ac61648550a99
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:10:08 +0300

    implement branch query

---
+++ Application/Handlers/Repositories/Queries/GetBranchQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Repositories.Queries
+{
+    public class GetBranchQuery : IRequest<BranchResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+        public string Branch { get; set; }
+    }
+
+    public class GetBranchQueryHandler : IRequestHandler<GetBranchQuery, BranchResponse>
+    {
+        private readonly IRepositoryService _repositoryService;
+
+        public GetBranchQueryHandler(IRepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        public Task<BranchResponse> Handle(GetBranchQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _repositoryService.GetBranch(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the branch", ex);
+            }
+        }
+    }
+}

5bfa891688c6c4411306d3acf13bb3bb9331b2be
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:08:22 +0300

    implement gitignore templates query

---
+++ Application/Handlers/Gitignores/Queries/GetGitignoreTemplatesQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Gitignores.Queries
+{
+    public class GetGitignoreTemplatesQuery : IRequest<List<GitignoreTemplateResponse>>
+    {
+    }
+
+    public class GetGitignoreTemplatesQueryHandler : IRequestHandler<GetGitignoreTemplatesQuery, List<GitignoreTemplateResponse>>
+    {
+        private readonly IGitignoreService _gitignoreService;
+
+        public GetGitignoreTemplatesQueryHandler(IGitignoreService gitignoreService)
+        {
+            _gitignoreService = gitignoreService;
+        }
+
+        public Task<List<GitignoreTemplateResponse>> Handle(GetGitignoreTemplatesQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gitignoreService.GetGitignoreTemplates(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the gitignore templates", ex);
+            }
+        }
+    }
+}

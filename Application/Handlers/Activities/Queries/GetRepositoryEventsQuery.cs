131f026166dd6df0db2d10a10c1808ec7ad885c5
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:05:36 +0300

    implement repository events query

---
+++ Application/Handlers/Activities/Queries/GetRepositoryEventsQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetRepositoryEventsQuery : IRequest<List<EventResponse>>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+    }
+
+    public class GetRepositoryEventsHandler : IRequestHandler<GetRepositoryEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _activityService;
+
+        public GetRepositoryEventsHandler(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetRepositoryEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _activityService.GetRepositoryEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the repository events", ex);
+            }
+        }
+    }
+}

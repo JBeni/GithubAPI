526582b3a594e85f62174d060371cb2ea9dc60fe
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:06:37 +0300

    implement gists query

--- Application/Handlers/Gists/Queries/GetGistsQuery.cs
+++ Application/Handlers/Gists/Queries/GetGistsQuery.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Application.Handlers.Gists.Queries
 {
-    public class GetGistsQuery
+    public class GetGistsQuery : IRequest<List<GistResponse>>
     {
+    }
+
+    public class GetGistsQueryHandler : IRequestHandler<GetGistsQuery, List<GistResponse>>
+    {
+        private readonly IGistService _gistService;
+
+        public GetGistsQueryHandler(IGistService gistService)
+        {
+            _gistService = gistService;
+        }
+
+        public Task<List<GistResponse>> Handle(GetGistsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gistService.GetGists(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the gists", ex);
+            }
+        }
     }
 }

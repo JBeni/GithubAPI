2cb31b8249ddeb3fe6b07bc9a901b7662fb77b47
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:05:51 +0300

    implement user public events query

---
+++ Application/Handlers/Activities/Queries/GetUserPublicEventsQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetUserPublicEventsQuery : IRequest<List<EventResponse>>
+    {
+        public string Username { get; set; }
+    }
+
+    public class GetUserPublicEventsHandler : IRequestHandler<GetUserPublicEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _activityService;
+
+        public GetUserPublicEventsHandler(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetUserPublicEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _activityService.GetUserPublicEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the user public events", ex);
+            }
+        }
+    }
+}

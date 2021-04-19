5e54f8ed94e302e387f8323a5a73a47389cb6956
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:11:09 +0300

    implement repository query

---
+++ Application/Handlers/Repositories/Queries/GetRepositoryQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Repositories.Queries
+{
+    public class GetRepositoryQuery : IRequest<RepositoryResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+    }
+
+    public class GetRepositoryQueryHandler : IRequestHandler<GetRepositoryQuery, RepositoryResponse>
+    {
+        private readonly IRepositoryService _repositoryService;
+
+        public GetRepositoryQueryHandler(IRepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        public Task<RepositoryResponse> Handle(GetRepositoryQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _repositoryService.GetRepository(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the repository", ex);
+            }
+        }
+    }
+}

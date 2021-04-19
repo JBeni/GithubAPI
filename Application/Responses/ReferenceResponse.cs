e307e952f925f96f31b5241b14ad429f2846360d
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:13:22 +0300

    implement reference response

---
+++ Application/Responses/ReferenceResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Responses
+{
+    public class ReferenceResponse
+    {
+        public string Ref { get; set; }
+        public string Node_id { get; set; }
+        public string Url { get; set; }
+        public ObjectGithub Object { get; set; }
+    }
+
+    public class ObjectGithub
+    {
+        public string Type { get; set; }
+        public string Sha { get; set; }
+        public string Url { get; set; }
+    }
+}

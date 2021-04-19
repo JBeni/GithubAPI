569f31ac9fe748f26e9eb958082f2dd7a74ebac1
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:08:37 +0300

    implement license query

---
+++ Application/Handlers/Licenses/Queries/GetLicenseQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Licenses.Queries
+{
+    public class GetLicenseQuery : IRequest<LicenseResponse>
+    {
+        public string License { get; set; }
+    }
+
+    public class GetLicenseQueryHandler : IRequestHandler<GetLicenseQuery, LicenseResponse>
+    {
+        private readonly ILicenseService _licenseService;
+
+        public GetLicenseQueryHandler(ILicenseService licenseService)
+        {
+            _licenseService = licenseService;
+        }
+
+        public Task<LicenseResponse> Handle(GetLicenseQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _licenseService.GetLicense(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the license", ex);
+            }
+        }
+    }
+}

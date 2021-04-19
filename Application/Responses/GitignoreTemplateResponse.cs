fa2e147a89d4d247fc5d4617cf3d4804859d63a7
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:12:24 +0300

    implement gitignore temaplate response

---
+++ Application/Responses/GitignoreTemplateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Responses
+{
+    public class GitignoreTemplateResponse
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+    }
+}

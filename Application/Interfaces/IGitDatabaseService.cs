7e40bf0a36bd37ebd2cc965086f334981041ceda
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:26:59 +0300

    create GitDatabase service

---
+++ Application/Interfaces/IGitDatabaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IGitDatabaseService
+    {
+        //Task<RequestResponse> CreateBlob(CreateBlobCommand command);
+        //Task<List<EventResponse>> GetBlob(GetBlobQuery query);
+        //Task<List<EventResponse>> GetCommit(GetCommitQuery query);
+        //Task<List<EventResponse>> GetReference(GetReferenceQuery query);
+        //Task<List<EventResponse>> GetTag(GetTagQuery query);
+    }
+}

937dcfd8ac13fdd9adae7c71699be9269725bff1
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:11:42 +0300

    implement Blob response

---
+++ Application/Responses/BlobResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Responses
+{
+    public class BlobResponse
+    {
+        public string Content { get; set; }
+        public string Encoding { get; set; }
+        public string Url { get; set; }
+        public string Sha { get; set; }
+        public int Size { get; set; }
+        public string Node_id { get; set; }
+    }
+}

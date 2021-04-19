569a219a6424e7bf9c75c81feeabaf997dbd261f
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:26:17 +0300

    create Action controller

--- WebUI/Controllers/ActionsController.cs
+++ WebUI/Controllers/ActionsController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
     public class ActionsController : ApiControllerBase
     {
+        protected HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public ActionsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("get-gists")]
         public async Task<IActionResult> GetGists()
         {
-            //var result = await Mediator.Send(query);
+            //string token = _configuration["GitHub:PersonalAccessToken"];
+            //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", token);
+
+
+            var response = await _httpClient.GetAsync("gists");
+            var content = await response.Content.ReadAsStringAsync();
+            var gists = JsonConvert.DeserializeObject<List<GistResponse>>(content);
+
             return Ok();
         }
     }

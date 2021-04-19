79b25805606c74deee8b8064b582fc10355d5cd5
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:07:19 +0300

    implement blob query

---
+++ Application/Handlers/GitDatabases/Queries/GetBlobQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.GitDatabases.Queries
+{
+    public class GetBlobQuery : IRequest<BlobResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+        public string FileSha { get; set; }
+    }
+
+    public class GetBlobQueryHandler : IRequestHandler<GetBlobQuery, BlobResponse>
+    {
+        private readonly IGitDatabaseService _gitDatabaseService;
+
+        public GetBlobQueryHandler(IGitDatabaseService gitDatabaseService)
+        {
+            _gitDatabaseService = gitDatabaseService;
+        }
+
+        public Task<BlobResponse> Handle(GetBlobQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gitDatabaseService.GetBlob(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the blob", ex);
+            }
+        }
+    }
+}

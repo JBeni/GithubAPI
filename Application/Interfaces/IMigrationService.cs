83a326bce4e73709c17e01275c4646d350e0eed7
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:16:20 +0300

    implement Migration service

--- Application/Interfaces/IMigrationService.cs
+++ Application/Interfaces/IMigrationService.cs
@@ -1,17 +1,13 @@
-﻿using System;
+﻿using Application.Handlers.Migrations.Queries;
+using Application.Responses;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
     public interface IMigrationService
     {
-        //Task<ArtifactResponse> GetOrganizationMigrations(GetOrganizationMigrationsQuery query);
-        //Task<ArtifactResponse> GetImportStatus(GetImportStatusQuery query);
-        //Task<ArtifactResponse> GetLargeFiles(GetLargeFilesQuery query);
-        //Task<ArtifactResponse> GetUserMigrations(GetUserMigrationsQuery query);
-        //Task<ArtifactResponse> GetUserMigrationStatus(GetUserMigrationStatusQuery query);
+        Task<ImportStatusResponse> GetImportStatus(GetImportStatusQuery query);
+        Task<List<UserMigrationResponse>> GetUserMigrations(GetUserMigrationsQuery query);
     }
 }
---
+++ Infrastructure/Services/MigrationService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Handlers.Migrations.Queries;
+using Application.Interfaces;
+using Application.Responses;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MigrationService : CommonService, IMigrationService
+    {
+        public async Task<ImportStatusResponse> GetImportStatus(GetImportStatusQuery query)
+        {
+            var response = await _httpClient.GetAsync($"repos/{query.Owner}/{query.Repo}/import");
+            var content = await response.Content.ReadAsStringAsync();
+            var results = JsonConvert.DeserializeObject<ImportStatusResponse>(content);
+            return results;
+        }
+
+        public async Task<List<UserMigrationResponse>> GetUserMigrations(GetUserMigrationsQuery query)
+        {
+            var response = await _httpClient.GetAsync($"user/migrations");
+            var content = await response.Content.ReadAsStringAsync();
+            var results = JsonConvert.DeserializeObject<List<UserMigrationResponse>>(content);
+            return results;
+        }
+    }
+}

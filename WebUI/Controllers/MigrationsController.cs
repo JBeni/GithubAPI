12f75bf137eb67a463d4b664f78e98bedee46928
JBeni <JBeni@users.noreply.github.com>
2021-04-19 19:10:39 +0300

    create methods for Migration controller

---
+++ WebUI/Controllers/MigrationsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Handlers.Migrations.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class MigrationsController : ApiControllerBase
+    {
+        [HttpGet("get-import-status")]
+        public async Task<IActionResult> GetImportStatus(GetImportStatusQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-user-migrations")]
+        public async Task<IActionResult> GetUserMigrations(GetUserMigrationsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}

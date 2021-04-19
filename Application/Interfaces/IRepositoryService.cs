3ae3c5e2a8ea9a78a79afe996a5c13ee1fffefc7
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:28:01 +0300

    create Repository Service

---
+++ Application/Interfaces/IRepositoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRepositoryService
+    {
+        //Task<List<EventResponse>> GetOrganizationRepositories(GetOrganizationRepositoriesQuery query);
+        //Task<List<EventResponse>> GetRepository(GetRepositoryQuery query);
+        //Task<List<EventResponse>> GetRepositoryContributors(GetRepositoryContributorsQuery query);
+        //Task<List<EventResponse>> GetBranches(GetBranchesQuery query);
+        //Task<List<EventResponse>> GetBranch(GetBranchQuery query);
+    }
+}

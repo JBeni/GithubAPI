57cad3c3b0e2c2ce256789d10be37ae561f26134
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:14:00 +0300

    implement tag response

---
+++ Application/Responses/TagResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Application.Responses
+{
+    public class TagResponse
+    {
+        public string Node_id { get; set; }
+        public string Tag { get; set; }
+        public string Sha { get; set; }
+        public string Url { get; set; }
+        public string Message { get; set; }
+        public Tagger Tagger { get; set; }
+        public ObjectGithub Object { get; set; }
+        public Verification Verification { get; set; }
+    }
+
+    public class Tagger
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}

e5a82096211321dcc1d40d440494be045fffa8cc
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:02:44 +0300

    implement GetArtifact query

--- Application/Handlers/Actions/Queries/GetArtifactQuery.cs
+++ Application/Handlers/Actions/Queries/GetArtifactQuery.cs
@@ -9,6 +9,8 @@ namespace Application.Handlers.Actions.Queries
 {
     public class GetArtifactQuery : IRequest<ArtifactResponse>
     {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
         public int ArtifactId { get; set; }
     }
 

c002ffb34bbd8a29779e6df18d90b6bfe86a2ce4
JBeni <JBeni@users.noreply.github.com>
2021-04-19 19:10:14 +0300

    create methods for GitDatabase controller

--- WebUI/Controllers/GitDatabasesController.cs
+++ WebUI/Controllers/GitDatabasesController.cs
@@ -1,14 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Handlers.GitDatabases.Queries;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
     public class GitDatabasesController : ApiControllerBase
     {
-        [HttpGet("get-gists")]
-        public async Task<IActionResult> GetGists()
+        [HttpGet("get-blob")]
+        public async Task<IActionResult> GetBlob(GetBlobQuery query)
         {
-            return Ok();
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-commit")]
+        public async Task<IActionResult> GetCommit(GetCommitQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-reference")]
+        public async Task<IActionResult> GetReference(GetReferenceQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("get-tag")]
+        public async Task<IActionResult> GetTag(GetTagQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
         }
     }
 }

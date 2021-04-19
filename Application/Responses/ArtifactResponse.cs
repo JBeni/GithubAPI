6ba1b4d345fd7d3ec983a73dedf753d5b1f8abc8
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:11:27 +0300

    implement Artifact response

--- Application/Responses/ArtifactResponse.cs
+++ Application/Responses/ArtifactResponse.cs
@@ -1,6 +1,18 @@
-﻿namespace Application.Responses
+﻿using System;
+
+namespace Application.Responses
 {
     public class ArtifactResponse
     {
+        public int Id { get; set; }
+        public string Node_id { get; set; }
+        public string Name { get; set; }
+        public int Size_in_bytes { get; set; }
+        public string Url { get; set; }
+        public string Archive_download_url { get; set; }
+        public bool Expired { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Expires_at { get; set; }
+        public DateTime Updated_at { get; set; }
     }
 }

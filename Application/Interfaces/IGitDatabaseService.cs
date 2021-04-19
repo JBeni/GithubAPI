8a1a0ab94de2df41fce4513121dfb7e10c481e5e
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:15:49 +0300

    implement GitDatabase service

--- Application/Interfaces/IGitDatabaseService.cs
+++ Application/Interfaces/IGitDatabaseService.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Application.Handlers.GitDatabases.Queries;
+using Application.Responses;
 using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
     public interface IGitDatabaseService
     {
-        //Task<RequestResponse> CreateBlob(CreateBlobCommand command);
-        //Task<List<EventResponse>> GetBlob(GetBlobQuery query);
-        //Task<List<EventResponse>> GetCommit(GetCommitQuery query);
-        //Task<List<EventResponse>> GetReference(GetReferenceQuery query);
-        //Task<List<EventResponse>> GetTag(GetTagQuery query);
+        Task<BlobResponse> GetBlob(GetBlobQuery query);
+        Task<CommitResponse> GetCommit(GetCommitQuery query);
+        Task<ReferenceResponse> GetReference(GetReferenceQuery query);
+        Task<TagResponse> GetTag(GetTagQuery query);
     }
 }
---
+++ Infrastructure/Services/GitDatabaseService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Handlers.GitDatabases.Queries;
+using Application.Interfaces;
+using Application.Responses;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class GitDatabaseService : CommonService, IGitDatabaseService
+    {
+        public async Task<BlobResponse> GetBlob(GetBlobQuery query)
+        {
+            var response = await _httpClient.GetAsync($"repos/{query.Owner}/{query.Repo}/git/blobs/{query.FileSha}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<BlobResponse>(content);
+            return result;
+        }
+
+        public async Task<CommitResponse> GetCommit(GetCommitQuery query)
+        {
+            var response = await _httpClient.GetAsync($"repos/{query.Owner}/{query.Repo}/git/commits/{query.CommitSha}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CommitResponse>(content);
+            return result;
+        }
+
+        public async Task<ReferenceResponse> GetReference(GetReferenceQuery query)
+        {
+            var response = await _httpClient.GetAsync($"repos/{query.Owner}/{query.Repo}/git/ref/{query.Ref}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReferenceResponse>(content);
+            return result;
+        }
+
+        public async Task<TagResponse> GetTag(GetTagQuery query)
+        {
+            var response = await _httpClient.GetAsync($"repos/{query.Owner}/{query.Repo}/git/tags/{query.TagSha}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TagResponse>(content);
+            return result;
+        }
+    }
+}

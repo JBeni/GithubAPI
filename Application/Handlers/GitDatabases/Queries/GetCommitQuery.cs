9956a59dd18c4cbdcaf034f10962583be182206c
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:07:27 +0300

    implement commit query

---
+++ Application/Handlers/GitDatabases/Queries/GetCommitQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.GitDatabases.Queries
+{
+    public class GetCommitQuery : IRequest<CommitResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+        public string CommitSha { get; set; }
+    }
+
+    public class GetCommitQueryHandler : IRequestHandler<GetCommitQuery, CommitResponse>
+    {
+        private readonly IGitDatabaseService _gitDatabaseService;
+
+        public GetCommitQueryHandler(IGitDatabaseService gitDatabaseService)
+        {
+            _gitDatabaseService = gitDatabaseService;
+        }
+
+        public Task<CommitResponse> Handle(GetCommitQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gitDatabaseService.GetCommit(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the commit", ex);
+            }
+        }
+    }
+}

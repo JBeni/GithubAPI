efae92aa68f49aca3b4702e01efde8d97eff14b0
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:08:45 +0300

    implement licenses query

---
+++ Application/Handlers/Licenses/Queries/GetLicensesQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Licenses.Queries
+{
+    public class GetLicensesQuery : IRequest<List<LicensesResponse>>
+    {
+    }
+
+    public class GetLicensesQueryHandler : IRequestHandler<GetLicensesQuery, List<LicensesResponse>>
+    {
+        private readonly ILicenseService _licenseService;
+
+        public GetLicensesQueryHandler(ILicenseService licenseService)
+        {
+            _licenseService = licenseService;
+        }
+
+        public Task<List<LicensesResponse>> Handle(GetLicensesQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _licenseService.GetLicenses(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the licenses", ex);
+            }
+        }
+    }
+}

28eb693887d058a0fd38c3cce786090048ec6a04
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:07:56 +0300

    implement tag query

---
+++ Application/Handlers/GitDatabases/Queries/GetTagQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.GitDatabases.Queries
+{
+    public class GetTagQuery : IRequest<TagResponse>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+        public string TagSha { get; set; }
+    }
+
+    public class GetTagQueryHandler : IRequestHandler<GetTagQuery, TagResponse>
+    {
+        private readonly IGitDatabaseService _gitDatabaseService;
+
+        public GetTagQueryHandler(IGitDatabaseService gitDatabaseService)
+        {
+            _gitDatabaseService = gitDatabaseService;
+        }
+
+        public Task<TagResponse> Handle(GetTagQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gitDatabaseService.GetTag(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the tag", ex);
+            }
+        }
+    }
+}

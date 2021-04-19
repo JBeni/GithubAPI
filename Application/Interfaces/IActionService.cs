610a75fa936e57810dba7a6c5766fb784397a702
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:25:31 +0300

    create Action Service

---
+++ Application/Interfaces/IActionService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Models;
+using Application.Handlers.Actions.Commands;
+using Application.Handlers.Actions.Queries;
+using Application.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IActionService
+    {
+        Task<ArtifactResponse> GetArtifact(GetArtifactQuery query);
+        Task<RequestResponse> DisableWorkflow(DisableWorkflowCommand command);
+        Task<RequestResponse> EnableWorkflow(EnableWorkflowCommand command);
+        Task<List<ArtifactResponse>> GetRepositoryArtifacts(GetRepositoryArtifactsQuery query);
+        Task<List<WorkflowResponse>> GetRepositoryWorkflows(GetRepositoryWorkflowsQuery query);
+        Task<WorkflowResponse> GetWorkflow(GetWorkflowQuery query);
+        Task<WorkflowResponse> GetWorkflowUsage(GetWorkflowUsageQuery query);
+    }
+}
---
+++ Infrastructure/Services/ActionService.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Models;
+using Application.Handlers.Actions.Commands;
+using Application.Handlers.Actions.Queries;
+using Application.Interfaces;
+using Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class ActionService : CommonService, IActionService
+    {
+        public Task<RequestResponse> DisableWorkflow(DisableWorkflowCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RequestResponse> EnableWorkflow(EnableWorkflowCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ArtifactResponse> GetArtifact(GetArtifactQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ArtifactResponse>> GetRepositoryArtifacts(GetRepositoryArtifactsQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<WorkflowResponse>> GetRepositoryWorkflows(GetRepositoryWorkflowsQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkflowResponse> GetWorkflow(GetWorkflowQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkflowResponse> GetWorkflowUsage(GetWorkflowUsageQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

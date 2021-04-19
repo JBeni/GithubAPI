0d8f70b323010ae285db7dc74754f20091fe97c5
JBeni <JBeni@users.noreply.github.com>
2021-04-19 08:45:15 +0300

    create Migration Service

---
+++ Application/Interfaces/IMigrationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IMigrationService
+    {
+        //Task<ArtifactResponse> GetOrganizationMigrations(GetOrganizationMigrationsQuery query);
+        //Task<ArtifactResponse> GetImportStatus(GetImportStatusQuery query);
+        //Task<ArtifactResponse> GetLargeFiles(GetLargeFilesQuery query);
+        //Task<ArtifactResponse> GetUserMigrations(GetUserMigrationsQuery query);
+        //Task<ArtifactResponse> GetUserMigrationStatus(GetUserMigrationStatusQuery query);
+    }
+}

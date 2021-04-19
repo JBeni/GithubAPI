86858b448c8dceac38edfac54ee7d0244852ebae
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:06:49 +0300

    implement public gists query

---
+++ Application/Handlers/Gists/Queries/GetPublicGistsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Gists.Queries
+{
+    public class GetPublicGistsQuery : IRequest<List<GistResponse>>
+    {
+    }
+
+    public class GetPublicGistsQueryHandler : IRequestHandler<GetPublicGistsQuery, List<GistResponse>>
+    {
+        private readonly IGistService _gistService;
+
+        public GetPublicGistsQueryHandler(IGistService gistService)
+        {
+            _gistService = gistService;
+        }
+
+        public Task<List<GistResponse>> Handle(GetPublicGistsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _gistService.GetPublicGists(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the public gists", ex);
+            }
+        }
+    }
+}

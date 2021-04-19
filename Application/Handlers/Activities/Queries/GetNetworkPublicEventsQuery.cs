9ada7f587355df3e347b204e816c42bde0e562ae
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:04:45 +0300

    implement network public events query

---
+++ Application/Handlers/Activities/Queries/GetNetworkPublicEventsQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetNetworkPublicEventsQuery : IRequest<List<EventResponse>>
+    {
+        public string Owner { get; set; }
+        public string Repo { get; set; }
+    }
+
+    public class GetNetworkPublicEventsHandler : IRequestHandler<GetNetworkPublicEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _activityService;
+
+        public GetNetworkPublicEventsHandler(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetNetworkPublicEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _activityService.GetNetworkPublicEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the network public events", ex);
+            }
+        }
+    }
+}

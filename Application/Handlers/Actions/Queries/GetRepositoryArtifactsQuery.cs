7cdc3c3e42065e8ac5f39a1230a2286fce2cd29f
JBeni <jitca.beniamin@gmail.com>
2021-04-19 08:23:15 +0300

    create GetRepositoryWorflows Query

---
+++ Application/Handlers/Actions/Queries/GetRepositoryArtifactsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Actions.Queries
+{
+    public class GetRepositoryArtifactsQuery : IRequest<List<ArtifactResponse>>
+    {
+    }
+
+    public class GetRepositoryArtifactsQueryHandler : IRequestHandler<GetRepositoryArtifactsQuery, List<ArtifactResponse>>
+    {
+        private readonly IActionService _actionService;
+
+        public GetRepositoryArtifactsQueryHandler(IActionService actionService)
+        {
+            _actionService = actionService;
+        }
+
+        public Task<List<ArtifactResponse>> Handle(GetRepositoryArtifactsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _actionService.GetRepositoryArtifacts(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the repository artifacts", ex);
+            }
+        }
+    }
+}

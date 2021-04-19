0d8f16e953061bc59947d112aed280f25b734c14
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:10:34 +0300

    implement organization repositories query

---
+++ Application/Handlers/Repositories/Queries/GetOrganizationRepositoriesQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Repositories.Queries
+{
+    public class GetOrganizationRepositoriesQuery : IRequest<List<RepositoryResponse>>
+    {
+        public string Organization { get; set; }
+    }
+
+    public class GetOrganizationRepositoriesQueryHandler : IRequestHandler<GetOrganizationRepositoriesQuery, List<RepositoryResponse>>
+    {
+        private readonly IRepositoryService _repositoryService;
+
+        public GetOrganizationRepositoriesQueryHandler(IRepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        public Task<List<RepositoryResponse>> Handle(GetOrganizationRepositoriesQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _repositoryService.GetOrganizationRepositories(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the organization repository", ex);
+            }
+        }
+    }
+}

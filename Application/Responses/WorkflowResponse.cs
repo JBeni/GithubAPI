28ed2dde7a3c62b76d9bf469d856a0ec6be3ed49
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:14:09 +0300

    implement workflow response

--- Application/Responses/WorkflowResponse.cs
+++ Application/Responses/WorkflowResponse.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Responses
 {
     public class WorkflowResponse
     {
+        public int Id { get; set; }
+        public string Node_id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string State { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public string Url { get; set; }
+        public string Html_url { get; set; }
+        public string Badge_url { get; set; }
     }
 }

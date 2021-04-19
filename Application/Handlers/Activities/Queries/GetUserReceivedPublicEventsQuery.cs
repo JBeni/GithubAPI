1fc03420ee8f0782137386871104a621a663d2b7
JBeni <JBeni@users.noreply.github.com>
2021-04-19 15:06:06 +0300

    implement user received public events query

---
+++ Application/Handlers/Activities/Queries/GetUserReceivedPublicEventsQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Activities.Queries
+{
+    public class GetUserReceivedPublicEventsQuery : IRequest<List<EventResponse>>
+    {
+        public string Username { get; set; }
+    }
+
+    public class GetUserReceivedPublicEventsQueryHandler : IRequestHandler<GetUserReceivedPublicEventsQuery, List<EventResponse>>
+    {
+        private readonly IActivityService _activityService;
+
+        public GetUserReceivedPublicEventsQueryHandler(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public Task<List<EventResponse>> Handle(GetUserReceivedPublicEventsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _activityService.GetUserReceivedPublicEvents(request);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an error retrieving the user received public events", ex);
+            }
+        }
+    }
+}
